c3a035aaf235e291285bf8d5db0cf26e8aba3de6
badalgeebin <badalgeebin@gmail.com>
2020-11-02 01:15:50 +0530

    added some json operations

--- src/infrastructure/content/ContentCommunicator.java
+++ src/infrastructure/content/ContentCommunicator.java
@@ -1,10 +1,21 @@
 package infrastructure.content;
 
+import org.json.JSONObject;
+
 public class ContentCommunicator implements IContentCommunicator{
+	
+	private String serverIPAddress;
+	protected String userName;
+	protected String userImage;
     
 	@Override
 	public void initialiseUser(String userDetails) {
     	
+		JSONObject jsonObject = new JSONObject(userDetails);
+		this.serverIPAddress = jsonObject.getString("ipAddress");
+		this.userName = jsonObject.getString("username");
+		this.userImage = jsonObject.getString("image");
+		
     }
 	
 	@Override
