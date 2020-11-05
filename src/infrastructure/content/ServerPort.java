037af24d5cd8954ebde215a349e77a054d6fd592
badalgeebin <badalgeebin@gmail.com>
2020-11-05 17:19:29 +0530

    ContentCommunicator class is completed and ServerPort class has been modified

--- src/infrastructure/content/ServerPort.java
+++ src/infrastructure/content/ServerPort.java
@@ -1,11 +1,27 @@
 package infrastructure.content;
 
+/**
+ * 
+ * @author Badal Kumar (111701008)
+ *
+ */
+
 public class ServerPort implements IServerPort {
-	/*
+	private int port = -1;
+	/**
 	 * This method will save the port of the board server locally to send message over networking
 	 */
-	@Override
 	public void sendPort(int port) {
-		
+		this.port = port;
+	}
+	
+	/**
+	 * This method will provide the port to its caller
+	 */
+	int getPort() {
+		if (port != -1) {
+			return port;
+		}
+		return -1;
 	}
 }
