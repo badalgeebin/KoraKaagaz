9e04f94e58fc41d0a855f52f8daeaf135b3c7124
badalgeebin <badalgeebin@gmail.com>
2020-11-10 17:54:23 +0530

    author and initial comments added

--- src/infrastructure/content/IServerPort.java
+++ src/infrastructure/content/IServerPort.java
@@ -1,10 +1,13 @@
 package infrastructure.content;
 
+/**
+ * This Interface is used for sending port of board server to content module.
+ * @author Badal Kumar (111701008)
+ */
 public interface IServerPort {
-	/*
+	/**
 	 * the processing module calls this method to send port of board server to each new client
-	 * @param : port, which is the port of board server in int datatype
+	 * @param port - This is the port of board server in int datatype
 	 */
 	void sendPort(int port);
-
 }
