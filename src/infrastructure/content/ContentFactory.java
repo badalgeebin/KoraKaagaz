e0a657d5040765320bb904ea6ac6d522f79c2055
badalgeebin <badalgeebin@gmail.com>
2020-11-10 19:21:22 +0530

    Proper comment added in this class

--- src/infrastructure/content/ContentFactory.java
+++ src/infrastructure/content/ContentFactory.java
@@ -1,12 +1,25 @@
 package infrastructure.content;
 
+/**
+ * This is a factory class of content module which contains instantiation of
+ * ContentCommunicator class and ServerPort class in two different methods from
+ * which other modules can get the object of those classes.
+ * @author Badal Kumar (111701008)
+ */
 public final class ContentFactory {
 	
 	private static IContentCommunicator instance1;
 	private static IServerPort instance2;
 	
+	/**
+	 * A private constructor so that another instance of ContentFactory can't be created.
+	 */
 	private ContentFactory() {}
 	
+	/**
+	 * This method will create an instance of ContentCommunicator class and returns it.
+	 * @return instance1 - An instance of ContentCommunicator class
+	 */
 	public static IContentCommunicator getContentCommunicator() {
 		if (instance1 == null) {
 			instance1 = new ContentCommunicator();
@@ -14,6 +27,10 @@ public final class ContentFactory {
 		return instance1;
 	}
 	
+	/**
+	 * This method will create an instance of ServerPort class and returns it.
+	 * @return instance2 - An instance of ServerPort class
+	 */
 	public static IServerPort getServerPort() {
 		if (instance2 == null) {
 			instance2 = new ServerPort();
