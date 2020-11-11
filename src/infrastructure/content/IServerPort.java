e670d96f32d4e21f3d85906128029e7ba9621a9f
badalgeebin <badalgeebin@gmail.com>
2020-11-12 03:24:55 +0530

    an additional method getPort() added

--- src/infrastructure/content/IServerPort.java
+++ src/infrastructure/content/IServerPort.java
@@ -2,6 +2,7 @@ package infrastructure.content;
 
 /**
  * This Interface is used for sending port of board server to content module.
+ * Moreover, that port value can be accessed too with a separate method. 
  * @author Badal Kumar (111701008)
  */
 public interface IServerPort {
@@ -10,4 +11,10 @@ public interface IServerPort {
 	 * @param port - This is the port of board server in int datatype
 	 */
 	void sendPort(int port);
+	
+	/**
+	 * This method will be used by content module itself to fetch the port value of server.
+	 * @return server's port value as integer
+	 */
+	int getPort();
 }
