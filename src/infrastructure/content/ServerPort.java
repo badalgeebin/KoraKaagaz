f086210af8cfad3f633379a6507b59946f8dda1a
badalgeebin <badalgeebin@gmail.com>
2020-11-23 21:42:28 +0530

    Modified a method name

--- src/infrastructure/content/ServerPort.java
+++ src/infrastructure/content/ServerPort.java
@@ -27,7 +27,7 @@ public class ServerPort implements IServerPort {
 	 * This method will save the port of the board server locally to send message over networking
 	 */
 	@Override
-	public void sendPort(int port) {
+	public void setPort(int port) {
 		this.port = port;
 	}
 	
